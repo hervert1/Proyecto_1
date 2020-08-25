36b5e6d607634ae82233cf55f1bcbfc63279daa0
Juan Carlos Salvador Hervert <jsalvador@GrupoGarflo.com>
2020-08-24 19:53:31 -0500

    formulario para producto

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,47 @@
 <!-- Main Content -->
 <div id="content">
 
-@include('default.search')
+  @include('default.search')
 
   <!-- Begin Page Content -->
   <div class="container-fluid">
 
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-      <h1 class="h3 mb-0 text-gray-800">Captura de usuario</h1>
+      <h1 class="h3 mb-0 text-gray-800">Productos</h1>
       <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
     </div>
 
-  
+    <div class="row">
+
+      <div class="col-lg-12">
+        <!-- Basic Card Example -->
+        <div class="card shadow mb-4">
+          <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-success">Nuevo Producto</h6>
+          </div>
+          <div class="card-body">
+            <div class="border-left-success">
+              <h5 class="ml-2">Información General</h5>
+            </div>
+            <hr>
+            <div class="border-left-success">
+              <h5 class="ml-2">Galeria de Imagenes</h5>
+            </div>
+            <hr>
+            <div class="border-left-success">
+              <h5 class="ml-2">Inventario</h5>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+
 
   </div>
   <!-- /.container-fluid -->
 
 
-@include('default.footer')
+  @include('default.footer')
