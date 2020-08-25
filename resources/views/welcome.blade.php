985213a1e83e857958253cd6cd70d17705b5b707
hervert1 <hervert123.jas@gmail.com>
2020-08-24 21:28:04 -0500

    Formulario productos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,19 +22,115 @@
           <div class="card-header py-3">
             <h6 class="m-0 font-weight-bold text-success">Nuevo Producto</h6>
           </div>
-          <div class="card-body">
-            <div class="border-left-success">
-              <h5 class="ml-2">Información General</h5>
+          <form>
+            <div class="card-body">
+              <div class="border-left-success">
+                <h5 class="ml-2">Información General</h5>
+              </div>
+              <hr>
+                <div class="form-row">
+                  <div class="form-group col-md-4">
+                    <label for="inputCodigo">Codigo</label>
+                    <input type="text" class="form-control" id="inputCodigo">
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputMarca">Marca</label>
+                    <input type="text" class="form-control" id="inputMarca">
+                  </div>
+                  <div class="form-group col-md-4">
+                    <label for="inputColor">Color</label>
+                    <input type="text" class="form-control" id="inputColor">
+                  </div>
+                </div>
+                <div class="form-group ">
+                  <label for="inputDescripcion">Descripcion</label>
+                  <textarea class="form-control" id="inputDescripcion" rows="3"></textarea>
+                </div>
+                <div class="form-row">
+                  <div class="form-group col-md-6">
+                    <label for="inputPrecio">Precio</label>
+                    <input type="number" class="form-control" id="inputPrecio">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="inputPrecioVenta">Precio Venta</label>
+                    <input type="number" class="form-control" id="inputPrecioVenta">
+                  </div>
+                </div>
+              <div class="border-left-success">
+                <h5 class="ml-2">Galeria de Imagenes</h5>
+              </div>
+              <hr>
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label for="inputImagen1">Imagen1</label>
+                  <input type="file" class="form-control-file" id="inputImagen1">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputImagen2">Imagen2</label>
+                  <input type="file" class="form-control-file" id="inputImagen2">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputImagen3">Imagen3</label>
+                  <input type="file" class="form-control-file" id="inputImagen3">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputImagen4">Imagen4</label>
+                  <input type="file" class="form-control-file" id="inputImagen4">
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputImagen5">Imagen5</label>
+                  <input type="file" class="form-control-file" id="inputImagen5">
+                </div>
+                </div>
+              <div class="border-left-success">
+                <h5 class="ml-2">Inventario</h5>
+              </div>
+              <hr>
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label for="inputTalla1">Talla</label>
+                  <input type="text" value="CH" class="form-control" id="inputTalla" readonly>
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputDisponible">Disponible</label>
+                  <input type="number" class="form-control" id="inputDisponible">
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label for="inputTalla2">Talla</label>
+                  <input type="text" value="M" class="form-control" id="inputTalla" readonly>
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputDisponible">Disponible</label>
+                  <input type="number" class="form-control" id="inputDisponible">
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label for="inputTalla3">Talla</label>
+                  <input type="text" value="L" class="form-control" id="inputTalla" readonly>
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputDisponible">Disponible</label>
+                  <input type="number" class="form-control" id="inputDisponible">
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-4">
+                  <label for="inputTalla">Talla</label>
+                  <input type="text" value="XL" class="form-control" id="inputTalla" readonly>
+                </div>
+                <div class="form-group col-md-4">
+                  <label for="inputDisponible">Disponible</label>
+                  <input type="number" class="form-control" id="inputDisponible">
+                </div>
+                </div>
+
+              </div>
+              <div class="d-flex justify-content-end pr-3"><button type="submit" class="btn btn-success mb-2 ">Guardar cambios</button> </div>
             </div>
-            <hr>
-            <div class="border-left-success">
-              <h5 class="ml-2">Galeria de Imagenes</h5>
-            </div>
-            <hr>
-            <div class="border-left-success">
-              <h5 class="ml-2">Inventario</h5>
-            </div>
-          </div>
+          </form>
         </div>
 
       </div>
